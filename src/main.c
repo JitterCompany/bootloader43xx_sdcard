a5dc5a36f45507445dc882b11a7c1392a604c360
S de Wit <dev@sjorsdewit.name>
2016-09-26 16:22:15 +0200

    link to system libraries, use gcc as linker
    This commit does not change how this example firmware project works,
    but makes it easier to build a more complex firmware on top of it:
    
    - link to libm, libc, libgcc. If you dou anything more than
        a very simple hello world program, you need this.
    - a few tweaks to the linker flags

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(void) {
     board_setup_pins();
     Chip_GPIO_SetPinState(LPC_GPIO_PORT, 0, 2, true);
     fpuInit();
-    
+
     while(1){
         for(volatile int n=0;n<10000000;n++){} 
         Chip_GPIO_SetPinState(LPC_GPIO_PORT, 0, 13, true);
