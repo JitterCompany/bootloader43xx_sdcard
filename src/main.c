d9669ae6dccb98b812a910931371953e12096522
S de Wit <dev@sjorsdewit.name>
2017-04-11 15:03:29 +0200

    Adjust boot leds
    UI:
    - 2x slow blink when the bootloader initializes
    - 10-20x fast blink when updating fails (10x for each core)
    - 5x slow blink after a succesfull update
    
    Note that after a succesfull update, the mcu is reset causing the
    bootloader to run again.

--- src/main.c
+++ src/main.c
@@ -17,17 +17,22 @@
 
 unsigned int stack_value = 0xA5A55A5A;
 
-#define BLINK_SLOW      (1500000)
-#define BLINK_FAST      (500000)
 
-static void blink(int count, int duration) {
+#define BLINK_SLOW      (400000)
+#define BLINK_FAST      (100000)
 
-    const GPIO *led = board_get_GPIO(GPIO_ID_LED_BLUE);
+static void blink(int count, int duration_us) {
+
+    const GPIO *led_board = board_get_GPIO(GPIO_ID_LED_BLUE);
+    const GPIO *led_ext = board_get_GPIO(GPIO_ID_EXT_LED_GREEN);
+
     for(int i=0;i<count;i++) {
-        for(volatile int n=0;n<duration;n++){} 
-        GPIO_HAL_set(led, HIGH);
-        for(volatile int n=0;n<duration;n++){} 
-        GPIO_HAL_set(led, LOW);
+        delay_us(duration_us);
+        GPIO_HAL_set(led_board,     HIGH);
+        GPIO_HAL_set(led_ext,       HIGH);
+        delay_us(duration_us);
+        GPIO_HAL_set(led_board,     LOW);
+        GPIO_HAL_set(led_ext,       LOW);
     }
 }
 
@@ -77,8 +82,8 @@ static void run_user_program(void)
 int main(void) {
     board_setup();
 
-    const GPIO *led = board_get_GPIO(GPIO_ID_LED_RED);
-    GPIO_HAL_set(led, HIGH);
+    GPIO_HAL_set(board_get_GPIO(GPIO_ID_LED_RED),       HIGH);
+    GPIO_HAL_set(board_get_GPIO(GPIO_ID_EXT_LED_RED),   HIGH);
 
     delay_init();
     sdcard_init(board_get_GPIO(GPIO_ID_SDCARD_POWER_ENABLE));
@@ -86,6 +91,7 @@ int main(void) {
 
     fpuInit();
 
+    // slow blink on startup
     blink(2, BLINK_SLOW);
 
     memset(&update_state, 0, sizeof(update_state));
@@ -93,16 +99,20 @@ int main(void) {
     check_fw("fw_m4.bin", FLASH_PROGRAM_M4_ADDR, FLASH_PROGRAM_M4_SIZE);
     check_fw("fw_m0.bin", FLASH_PROGRAM_M0_ADDR, FLASH_PROGRAM_M0_SIZE);
 
+    // fast blinking if firmware could not be updated
     blink(10*update_state.error_count, BLINK_FAST);
 
     if(update_state.updated) {
-        blink(10, BLINK_SLOW);
+        // slow blink after an update
+        blink(5, BLINK_SLOW);
+
         // reset mcu
         delay_us(200000);
         Chip_RGU_TriggerReset(RGU_CORE_RST);
     }
     
-    GPIO_HAL_set(led, LOW);
+    GPIO_HAL_set(board_get_GPIO(GPIO_ID_LED_RED),       LOW);
+    GPIO_HAL_set(board_get_GPIO(GPIO_ID_EXT_LED_RED),   LOW);
     
     // NOTE: disable all interrupts that this bootloader has enabled
     // to avoid unexpected calling of user IRQHandlers
